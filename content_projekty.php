fe7d30dba4c1d4119d958bec57f5df6404f8ec1a
myusername <kierej@gmail.com>
2017-02-21 02:09:53 +0100

    projekty/wydarzenia klikniecie na zdjecie wydarzenia powoduje przejscie do szczegolow wydarzenia

--- content_projekty.php
+++ content_projekty.php
@@ -1,6 +1,7 @@
 <div class="events--content ">   
-    <?php   the_post_thumbnail(); ?>          
-    
+    <a href="<?php the_permalink(); ?>">
+      <?php   the_post_thumbnail(); ?>          
+    </a>
     <div class="event--content-descrip">
     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
        <time datetime=""><?php echo CFS()->get( 'data_projekty' );?></time>
