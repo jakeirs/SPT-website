fdebb10e05e0af6f3aa815dafc17fb5f19852b17
myusername <kierej@gmail.com>
2017-02-21 03:16:03 +0100

    test

--- content_projekty.php
+++ content_projekty.php
@@ -1,9 +1,9 @@
 <div class="events--content ">   
     <a href="<?php the_permalink(); ?>">
-      <?php   the_post_thumbnail(); ?>          
+      <?php   the_post_thumbnail(); ?>  
+      <div class="event--content-descrip">
+      <h3><?php the_title(); ?></h3>
     </a>
-    <div class="event--content-descrip">
-    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
        <time datetime=""><?php echo CFS()->get( 'data_projekty' );?></time>
        <p class="event--content-tags">
        
@@ -12,4 +12,3 @@
   </div>
          
        
-  
